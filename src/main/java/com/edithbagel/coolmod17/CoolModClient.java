3e38f78e14d2cb21638b7d1673f5b39a7ca45dd9
DrPHDCat <frizy8789@gmail.com>
2024-10-03 20:48:40 +0600

    added some very basic stuff, i think

---
+++ src/main/java/com/edithbagel/coolmod17/CoolModClient.java
@@ -0,0 +1,12 @@
+package com.edithbagel.coolmod17;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+
+@Mod("coolmod17")
+public class CoolModClient {
+    public CoolModClient(FMLModContainer container, IEventBus modBus)
+    {
+
+    }
+}
